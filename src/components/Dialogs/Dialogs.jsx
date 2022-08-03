fe434fadd44398f3074b025999158dd666d97395
kolka72 <kolia199972@gmail.com>
2022-08-03 23:22:01 +0300

    21. create content Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-// import s from './Dialogs.module.css'
+import s from './Dialogs.module.css'
 
 const Dialogs = () => {
-  return <div>Dialogs</div>
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>Kolya</div>
+        <div className={s.dialog}>Julia</div>
+        <div className={s.dialog}>Kirill</div>
+        <div className={s.dialog}>Dasha</div>
+        <div className={s.dialog}>Sveta</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>My name</div>
+        <div className={s.message}>Yo</div>
+      </div>
+    </div>
+  )
 }
 
 export default Dialogs
